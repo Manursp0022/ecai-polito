3fe54c0bba4c7a5d7ec8fe1693919515c4c21a46
Manursp0022 <rosapepemanu0022@gmail.com>
2025-10-15 21:36:30 +0200

    lab1-lab2 exercises

--- lab1/lab1_ex1.py
+++ lab1/lab1_ex1.py
@@ -1,28 +1,44 @@
+"""
+a.	Create a new Python file (e.g., lab1_ex2.py) and write a script that uses the adafruit_dht module to read temperature and humidity from the DHT-11 sensor. 
+
+b.	Every 2 seconds, print the T/H values using the following format:
+year-month-day hour:minute:second.microseconds - mac_address:temperature = T
+year-month-day hour:minute:second.microseconds - mac_address:humidity = H
+
+where mac_address is the MAC address of your network card, 
+T is the temperature value in degree Celsius, 
+and H is the humidity value in %RH.
+
+Example:
+2022-10-01 19:21:51.699254 - 0xf0b61e0bfe09:temperature = 21
+2022-10-01 19:21:51.699254 - 0xf0b61e0bfe09:humidity = 60
+2022-10-01 19:21:53.701326 - 0xf0b61e0bfe09:temperature = 22
+2022-10-01 19:21:53.701326 - 0xf0b61e0bfe09:humidity = 59
+"""
+
+
 import adafruit_dht
 import uuid
 import time
 from datetime import datetime
 from board import D4
 
-mac_adress = hex(uuid.getnode())
-print(mac_adress)
-
+mac_address = hex(uuid.getnode())
 dht_device = adafruit_dht.DHT11(D4)
 
 while True:
     timestamp = time.time()
-    print(timestamp)
-    formatted_date = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S.%f')
+    formatted_datetime = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S.%f')
     try:
         temperature = dht_device.temperature
         humidity = dht_device.humidity
-        print(f'{formatted_date}-{mac_adress}:{temperature}')
-        print(f'{formatted_date}-{mac_adress}:{humidity}')
-        print()
+
+        print(f'{formatted_datetime} - {mac_address}:temperature = {temperature}')
+        print(f'{formatted_datetime} - {mac_address}:humidity = {humidity}')
     except:
-        print()
+        print(f'{formatted_datetime} - sensor failure')
         dht_device.exit()
         dht_device = adafruit_dht.DHT11(D4)
 
-    print(formatted_date)
-    time.sleep(2)
+    
+    time.sleep(1)
--- lab1/lab1_ex2.py
+++ lab1/lab1_ex2.py
@@ -1,26 +1,33 @@
-import sounddevice as sd 
+"""
+c.	Modify the script to let the user to disable/enable the audio storage by pressing P key.
+"""
+
+
+import os
+import sounddevice as sd
 from time import time
 from scipy.io.wavfile import write
-import os
-#Samplerate: how much sample we collect each second
 
-"""
-Modify the script to store the audio data every second. Use the stream callback function to store
-data in parallel while recording. Use the scipy.io.wavfile.write function to store the audio data.
-Use the timestamp of the recording as the filename
-"""
-#indata will be populated by soundevice with the recorded data
-def callback(indata,frames,callback_time,status):
-    timestamp = time()
-    write(f'{timestamp}.wav',48000,indata)
-    filesize_in_bytes = os.path.getsize(f'{timestamp}.wav')
-    filesize_in_Kbytes = filesize_in_bytes / 1024
-    print(f'Size:{filesize_in_Kbytes:.2f}KB')
 
-with sd InputStream(device=1,channels=1,dtype='int32',samplerate=48000,
-callback=callback,blockSize=48000):  #samplerate = 48000 because we want to execute each second
-    while True: 
+def callback(indata, frames, callback_time, status):
+    """This is called (from a separate thread) for each audio block."""
+    global store_audio
+
+    if store_audio is True:
+        timestamp = time()
+        write(f'{timestamp}.wav', 48000, indata)
+        filesize_in_bytes = os.path.getsize(f'{timestamp}.wav')
+        filesize_in_kb = filesize_in_bytes / 1024
+        print(f'Size: {filesize_in_kb:.2f}KB')
+
+
+store_audio = True
+
+with sd.InputStream(device=1, channels=1, dtype='int32', samplerate=48000, blocksize=48000, callback=callback):
+    while True:
         key = input()
-        if key in ('q','Q'):
+        if key in ('q', 'Q'):
             print('Stop recording.')
-        #in this version we don't store the audio data
+            break
+        if key in ('p', 'P'):
+            store_audio = not store_audio
